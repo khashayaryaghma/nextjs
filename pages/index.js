74713e34da9ae747b41eac0a94ceddcec0323393
khashayaryaghma <92808021+khashayaryaghma@users.noreply.github.com>
2022-10-27 18:56:34 +0330

    update

---
+++ pages/index.js
@@ -0,0 +1,12 @@
+import Head from 'next/head'
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>To-do List</title>
+      </Head>
+      <h1>HomePage</h1>
+    </>
+  )
+}
